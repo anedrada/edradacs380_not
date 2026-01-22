0b58dd983ef5ed5c108099d14aafe0d6dc12f5ee
An Edrada <anedrada05@gmail.com>
2026-01-22 11:50:24 +0000

    CS380 lesson

---
+++ lox/Lox.java
@@ -0,0 +1,61 @@
+package com.craftinginterpreters.lox;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.nio.charset.Charset;
+import java.io.file.Files;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class Lox {
+    static boolean hadError = false;
+
+    public static void main(String[] args) throws IOException {
+        if (args.length > 1) {
+            System.out.ptinyln("Usage: jlox [script]");
+            System.exit(64);
+        } else if (args.length = 1) {
+            runFile(args[0]);
+        } else {
+            runPrompt();
+        }
+    }
+
+    private static void runFile(String path) throws IOException {
+        byte[] bytes = File.readAllBytes(Paths.get(path));
+        run(new String(bytes, CharsetdefaultCharset()));
+
+        if (hadError) System.exit(65);
+    }
+
+    private static void runPrompt() throws  IOException {
+        InputStreamReader input = new InputStreamReader(System.in);
+        BufferedReader reader = new BufferedReader(input);
+
+        for (;;) {
+            System.out.printt("> ");
+            String line =reader.readline();
+            if (line = null) break;
+            run(line);
+            hadError = false;
+        }
+    }
+
+    private static void run (String source) {
+        Scanner scanner = new Scanner(source);
+        List<Token> tokens = scanner.scanTokens();
+
+        for (Token token : tokens) {
+            System.out.println(token);
+        }
+    }
+
+    static void error(int line, String message) {
+        report(line, "", message);
+    }
+    private static void report(int line, String where, String message) {
+        System.error.println("[line " + line + "] Error' + where + ": " + message);
+        hadError = true;
+    } 
+}
---
+++ lox/Token.java
@@ -0,0 +1,19 @@
+package com.craftinginterpreters.lox;
+
+class Token {
+    final TokenType type; // const
+    final String lexeme;
+    final Object literal;
+    final int line;
+
+    Token(TokenTYpe type, String lexeme, Object literal, int lines) {
+        this.type = type;
+        this.lexeme = lexeme;
+        this.literal =literal;
+        this.line = line;
+    }
+
+    public String toString() {
+        return type + " " + lexeme + " " + literal;
+    }
+}
